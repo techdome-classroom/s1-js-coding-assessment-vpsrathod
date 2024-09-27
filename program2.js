28500fd23f085ad937b2d087133f020076242a6e
Vishnu Pratap Singh Rajput <vishnurp01234@gmail.com>
2024-09-27 12:52:05 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,23 @@
 const decodeTheRing = function (s, p) {
+    let memo = {};
 
-    // write your code here
+    function dfs(i, j) {
+        if (memo.hasOwnProperty(`${i},${j}`)) return memo[`${i},${j}`];
+        if (i === s.length && j === p.length) return true;
+        if (j === p.length) return false;
 
-  };
-  
-  module.exports = decodeTheRing;
+        let match = i < s.length && (s[i] === p[j] || p[j] === '?');
+
+        if (p[j] === '*') {
+            memo[`${i},${j}`] = dfs(i, j + 1) || (i < s.length && dfs(i + 1, j));
+        } else {
+            memo[`${i},${j}`] = match && dfs(i + 1, j + 1);
+        }
+
+        return memo[`${i},${j}`];
+    }
+
+    return dfs(0, 0);
+};
+
+module.exports = decodeTheRing;
